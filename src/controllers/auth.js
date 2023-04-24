4390d844087621e4435505fb3f275809d67ce85a
Goodnews Sandy <54219127+sandygudie@users.noreply.github.com>
2023-04-24 12:44:20 +0100

    fix email bug

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -81,7 +81,7 @@ const verifyUser = async (req, res) => {
   if (user) {
     user.isVerified = 'Verified'
     await user.save()
-    return successResponse(res, 200, 'Email Verified Sucessfully!')
+    return successResponse(res, 200, 'Email Verification Sucessfully!')
   }
   return errorResponse(res, 404, 'Invalid link')
 }
