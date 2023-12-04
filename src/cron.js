d066da1e9e1d09cfedd9da60ea7aacea380485f3
sandygudie <goodnewssandy@gmail.com>
2023-12-04 19:44:56 +0100

    added cron jobs

---
+++ src/cron.js
@@ -0,0 +1,19 @@
+const cron = require('node-cron')
+const https = require('https')
+
+const backendUrl = 'https://quizbase.onrender.com/api/v1'
+const job = cron.schedule('*/14 * * * *', function () {
+  https
+    .get(backendUrl, (res) => {
+      if (res.statusCode === 200) {
+        console.log('Server restarted')
+      } else {
+        console.error(`Failed to restart with with status code: ${res.statusCode}`)
+      }
+    })
+    .on('error', (err) => {
+      console.error(`Error during restart: ${err.message}`)
+    })
+})
+
+module.exports = { job }
--- src/server.js
+++ src/server.js
@@ -2,7 +2,9 @@ require('dotenv').config()
 const app = require('./app')
 const logger = require('./config/logger')
 const { PORT } = require('./config/config')
+const { job } = require('./cron')
 
+job.start()
 app.listen(PORT, () => {
   logger.info(`Server running on port ${PORT}`)
 })
